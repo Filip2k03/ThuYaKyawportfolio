70bf37f9c82df7c116056a19ee853fc1680af00d
Filip2k03 <stephanfilip7@gmail.com>
2025-02-17 21:22:45 +0630

    skill

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,42 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import SkillsSection from '../components/SkillsSection';
 import { FaLinkedin, FaGithub, FaFacebook, FaInstagram } from 'react-icons/fa';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Portfolio Website</title>
         <meta name="description" content="Welcome to my portfolio website." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <section className="home">
-        <div className="home-img">
-          <img src="/images/codecraft.jpg" alt="CodeCraft" className="rounded-img" />
+      <section className={styles.home}>
+        <div className={styles.homeImg}>
+          <Image src="/images/codecraft.jpg" alt="CodeCraft" className={styles.roundedImg} width={300} height={300} />
         </div>
-        <div className="home-content">
+        <div className={styles.homeContent}>
           <h1>Hi, It's <span>ThuYaKyaw</span></h1>
-          <h3 className="typing-text">I'm a <span></span></h3>
+          <h3 className={styles.typingText}>I'm a <span></span></h3>
           <p>I am a freelancer</p>
-          <a href="./download/cv.pdf" className="btn" download="cv_of_thuyakyaw">Download CV</a>
-          <a href="./skills" className="btn">Skills</a>
-          <div className="social-icons">
+          <div className={styles.buttons}>
+            <a href="./download/cv.pdf" className={styles.btn} download="cv_of_thuyakyaw">Download CV</a>
+            <a href="./skills" className={styles.btn}>Skills</a>
+          </div>
+          <div className={styles.socialIcons}>
             <a href="https://www.linkedin.com/in/thu-ya-kyaw-5a606732b"><FaLinkedin /></a>
             <a href="https://github.com/Filip2k03"><FaGithub /></a>
             <a href="#"><FaFacebook /></a>
             <a href="#"><FaInstagram /></a>
           </div>
-          <button id="contactButton" className="btn">Hire me</button>
+          <button id="contactButton" className={styles.btn}>Hire me</button>
         </div>
       </section>
+      <SkillsSection />
       <Footer />
     </div>
   );
